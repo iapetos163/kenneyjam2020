1ac1506250aadf251ad8d6e0a8331e348d4c6d4e
Brett Houtz <bcat38@gmail.com>
2020-08-22 13:23:29 -0700

    Render walls

--- src/AssetStore.ts
+++ src/AssetStore.ts
@@ -1,8 +1,9 @@
 import panelingNE from './assets/world/paneling_NE.png';
+import panelingNW from './assets/world/paneling_NW.png';
 
 const ERR_NOT_READY = new Error('Cannot get image from AssetStore because it is not ready');
 
-const images: {[key: string]: HTMLImageElement} = {};
+const images: {[key: string]: CanvasImageSource} = {};
 const loadPromises: Promise<void>[] = [];
 let ready = false;
 
@@ -19,12 +20,18 @@ function loadImage(path: string, key: string): void {
 
 export async function load(): Promise<void> {
   loadImage(panelingNE, 'panelingNE');
+  loadImage(panelingNW, 'panelingNW');
+  await Promise.all(loadPromises);
   ready = true;
 }
 
-export function get(key: string): HTMLImageElement | undefined {
+export function get(key: string): CanvasImageSource {
   if (!ready) {
     throw ERR_NOT_READY;
   }
-  return images[key];
+  const img = images[key];
+  if (img === undefined) {
+    throw new Error(`Asset ${key} not fonud`);
+  }
+  return img;
 }
--- src/UIManager.ts
+++ src/UIManager.ts
@@ -1,10 +1,21 @@
 import Loading from './Loading';
-import { load as loadAssets } from './AssetStore';
+import { load as loadAssets, get as getAsset } from './AssetStore';
+
+const GAME_WIDTH = 960;
+const GAME_HEIGHT = 640;
+
+function wallProject(x: number, y: number, z: number, xOriginOffset = 0): [number, number] {
+  return [
+    480 - xOriginOffset - (52 * x) + (52 * y),
+    320 - 64 - (61 * z) + (37 * (x + y))
+  ];
+}
 
 export default class UIManager {
   private readonly gameElem: HTMLDivElement;
-  private readonly errorElem = document.createElement('p');
+  private readonly errorElem = document.createElement('div');
   private readonly loadingLayer: Loading;
+  private readonly ctx: CanvasRenderingContext2D;
   private updatePending = false;
   private errored = false;
 
@@ -15,11 +26,46 @@ export default class UIManager {
     game.appendChild(this.errorElem);
 
     this.loadingLayer = new Loading(game);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = GAME_WIDTH;
+    canvas.height = GAME_HEIGHT;
+    game.appendChild(canvas);
+    const ctx = canvas.getContext('2d');
+
+    if (ctx === null) {
+      this.fail('Failed to create 2D context');
+      this.ctx = new CanvasRenderingContext2D();
+      return;
+    }
+    this.ctx = ctx;
   }
 
   private fail(reason: string): void {
     this.errored = true;
     this.errorElem.innerText = reason;
+  }
+
+  private renderWallNE(): void {
+    const panelingNE = getAsset('panelingNE');
+
+    for (let x = 0; x < 6; x++) {
+      for (let z = 0; z < 3; z++) {
+        const [pX, pZ] = wallProject(x, 0, z, 55);
+        this.ctx.drawImage(panelingNE, pX, pZ);
+      }
+    }
+  }
+
+  private renderWallNW(): void {
+    const panelingNE = getAsset('panelingNW');
+
+    for (let y = 0; y < 6; y++) {
+      for (let z = 0; z < 3; z++) {
+        const [pY, pZ] = wallProject(0, y, z);
+        this.ctx.drawImage(panelingNE, pY, pZ);
+      }
+    }
   }
 
   public draw(): void {
@@ -31,15 +77,9 @@ export default class UIManager {
   }
 
   public init(): void {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-
-    if (ctx == null) {
-      this.fail('Failed create 2D context');
-      return;
-    }
-
     loadAssets().then(() => {
+      this.renderWallNE();
+      this.renderWallNW();
       this.loadingLayer.active = false;
     }).catch(err => {
       const message = err?.message;
